3099e899d2182055b4f67ac6f95c225ef9f5da1a
Germán Altairac <germanaltairac@hotmail.com>
2021-12-27 20:06:12 -0300

    initial commit

---
+++ src/main/kotlin/cl/assachile/ProductWeightHandler/ProductWeightHandlerApplication.kt
@@ -0,0 +1,11 @@
+package cl.assachile.ProductWeightHandler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductWeightHandlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductWeightHandlerApplication>(*args)
+}
